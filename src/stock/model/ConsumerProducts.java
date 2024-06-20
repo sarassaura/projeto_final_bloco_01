160db7022b790c0f62949c36a5546c440750f036
sarassaura <sarahyukinonakada@gmail.com>
2024-06-20 11:54:53 -0300

    feat: implement consumer and industrial products

---
+++ src/stock/model/ConsumerProducts.java
@@ -0,0 +1,29 @@
+package stock.model;
+
+import stock.util.Format;
+
+public class ConsumerProducts extends Product {
+	
+	private String client;
+
+	public ConsumerProducts(String id, String name, int totalSales, int availableProducts, float price, String category,
+			String brand, String client) {
+		super(id, name, totalSales, availableProducts, price, category, brand);
+		this.client = client;
+	}
+
+	public String getClient() {
+		return client;
+	}
+
+	public void setClient(String client) {
+		this.client = client;
+	}
+	
+	@Override
+	public void showProduct() {
+		super.showProduct();
+		Format.text("Client's name: " + this.client, 0, 2, false);
+	}
+
+}
